8c8779076edf678fddcb470830749938282b07cd
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2023-02-22 20:43:18 -0600

    Delete autopartsclient_ directory

--- autopartsclient_/SharedVaribles.java
+++
@@ -1,9 +0,0 @@
-package autopartsclient;
-
-public class SharedVaribles {
-	public static boolean showArrayList = true;
-	public static boolean showFPS = true;
-	public static boolean showCords = true;
-	public static boolean doChatMod = true;
-	public static boolean showTPS = true;
-}
